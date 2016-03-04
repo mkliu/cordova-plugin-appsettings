9de218ddc9cd0aa9fe9cf49a4e66268a71c12f03
Sino <SinoBoeckmann@users.noreply.github.com>
2016-03-04 17:32:28 +0100

    Let the error callback be an alternative option
    You're not in need to have an errorCallback always be necessary

--- www/appsettings.js
+++ www/appsettings.js
@@ -8,12 +8,17 @@ function AppSettings() {
 /**
  * Get a config.xml settings (preference) value
  *
+ * @param {String} keyArray Key
  * @param {Function} successCallback The function to call when the value is available
  * @param {Function} errorCallback The function to call when value is unavailable
- * @param {String} key Key
  */
-AppSettings.prototype.get = function (successCallback, errorCallback, keyArray) {
-    argscheck.checkArgs('fFa', 'AppSettings.get', arguments);
+AppSettings.prototype.get = function (keyArray, successCallback, errorCallback) {
+    try {
+        argscheck.checkArgs('AFF', 'AppSettings.get', arguments);
+    }
+    catch () {
+       argscheck.checkArgs('AF', 'AppSettings.get', arguments); 
+    }
 
     exec (successCallback, errorCallback, "AppSettings", "get", keyArray);
 };
